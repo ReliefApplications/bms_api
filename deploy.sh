d1453341bcf3fcc0c9429f98deff09a6e402706f
Barbara <barbara.reliefapps@gmail.com>
2019-03-01 17:07:28 +0700

    test travis deploy to test server

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ $1 == "prod" ]]; then
+    $ec2 = ubuntu@ec2-35-158-182-63.eu-central-1.compute.amazonaws.com
+elif [[ $1 == "staging" ]]; then
+    $ec2 = ubuntu@ec2-52-57-90-156.eu-central-1.compute.amazonaws.com
+else
+    echo "Unknown environment"
+    exit
+fi
+
+ssh -i $2 $ec2 \ 
+    "cd /var/www/html/bms_api; \
+    git pull origin $1; \
+    sudo docker-compose exec php sf c:c \
+    sudo docker-compose exec php sf d:m :m -n"
