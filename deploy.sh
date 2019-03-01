2d13e615649619540c5ccc2a581ac3c920491046
Barbara <barbara.reliefapps@gmail.com>
2019-03-01 18:29:58 +0700

    fix travis file

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [[ $1 == "prod" ]]; then
-    ec2="ubuntu@ec2-35-158-182-63.eu-central-1.compute.amazonaws.com"
+    ec2="ec2-35-158-182-63.eu-central-1.compute.amazonaws.com"
 elif [[ $1 == "staging" ]]; then
-    ec2="ubuntu@ec2-52-57-90-156.eu-central-1.compute.amazonaws.com"
+    ec2="ec2-52-57-90-156.eu-central-1.compute.amazonaws.com"
 else
     echo "Unknown environment"
     exit
@@ -12,7 +12,7 @@ if [ -z `ssh-keygen -F $ec2` ]; then
   ssh-keyscan -H $ec2 >> ~/.ssh/known_hosts
 fi
 
-ssh -i $2 $ec2 \ 
+ssh -i $2 "ubuntu@$ec2" \ 
     "cd /var/www/html/bms_api; \
     git pull origin dev; \
     sudo docker-compose exec php sf c:c \
