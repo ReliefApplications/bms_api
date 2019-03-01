0cf83889b0ffc25fa3bf5ca901d558d91c70e07a
Barbara <barbara.reliefapps@gmail.com>
2019-03-01 17:27:10 +0700

    test deploy

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [[ $1 == "prod" ]]; then
-    $ec2 = ubuntu@ec2-35-158-182-63.eu-central-1.compute.amazonaws.com
+    $ec2="ubuntu@ec2-35-158-182-63.eu-central-1.compute.amazonaws.com"
 elif [[ $1 == "staging" ]]; then
-    $ec2 = ubuntu@ec2-52-57-90-156.eu-central-1.compute.amazonaws.com
+    $ec2="ubuntu@ec2-52-57-90-156.eu-central-1.compute.amazonaws.com"
 else
     echo "Unknown environment"
     exit
@@ -11,6 +11,6 @@ fi
 
 ssh -i $2 $ec2 \ 
     "cd /var/www/html/bms_api; \
-    git pull origin $1; \
+    git pull origin dev; \
     sudo docker-compose exec php sf c:c \
     sudo docker-compose exec php sf d:m :m -n"
